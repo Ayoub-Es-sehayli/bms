0fe370803c3119a9b0c59526aef0f2b61b4532e3
Es-sehayli Ayoub <ayoub1essehayli@gmail.com>
2025-02-11 18:03:24 +0100

    Added BMS Client Service
    - Created .dockerignore
    - Created Dockerfile
    - Updated Compose file
    - Updated run.sh script

--- run.sh
+++ run.sh
@@ -3,16 +3,5 @@
 # Empty Logs
 rm logs/*
 
-set PROJECT_DIR $PWD
-# Build Server Project
-cd bms-server
-eval $(pdm venv activate)
-pdm build
-
-cd $PROJECT_DIR
-
 # Run Server Container
-sudo docker compose --env-file .env up -d --build api.bms
-sudo docker compose logs -f api.bms
-sudo docker compose down api.bms
-bat logs/*
+sudo docker compose --env-file .env up --watch --build client.bms
